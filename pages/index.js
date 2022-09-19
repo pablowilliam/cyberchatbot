bdf81f099b48dfff06e460d778990e5d2cbf6d00
Milton Araújo <miltonjr.dev@gmail.com>
2022-09-18 22:36:24 -0300

    feat: section price

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import Banner from "../components/Banner";
 import Footer1 from "../components/Footer1";
 import Navbar from "../components/Navbar";
+import Price from "../components/Prices";
 
 const HomePage = () => {
   return (
     <div>
       <Navbar />
       <Banner />
+      <Price />
       <Footer1 />
     </div>
   );
